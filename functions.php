f189997bf3282b38e9c857edad746b02a1b46e92
jpfm85 <joaopaulofm.si@gmail.com>
2014-11-05 10:55:57 -0200

    Commit Sprint 6

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 if ( !isset( $content_width ) ) {
 	$content_width = 900;
 }
--- src/bookpress/theme.php
+++ src/bookpress/theme.php
@@ -10,8 +10,14 @@ namespace BookPress;
 class Theme {
 
 	public function __construct() {
+
 		add_action( 'after_setup_theme', array( __CLASS__, 'after_setup_theme' ) );
-		add_filter('comment_form_default_fields', array(__CLASS__, 'comment_form_default_fields'));
+		add_action( 'template_redirect', array( __CLASS__, 'template_redirect' ) );
+
+		add_filter( 'search_queries', array( __CLASS__, 'search_queries' ), 10, 1 );
+		add_filter( 'comment_form_default_fields', array( __CLASS__, 'comment_form_default_fields' ) );
+		add_filter( 'pre_get_posts', array( __CLASS__, 'pre_get_posts' ), 10, 1 );
+
 		require_once get_template_directory() . '/src/bookpress/types/item.php';
 		require_once get_template_directory() . '/src/bookpress/taxonomies/author.php';
 		require_once get_template_directory() . '/src/bookpress/metaboxes/item-info.php';
@@ -25,11 +31,94 @@ class Theme {
 		add_theme_support( 'post-thumbnails' );
 		load_theme_textdomain( 'bookpress', get_template_directory() . '/languages' );
 	}
-		public static function comment_form_default_fields($fields) {
-			unset($fields ['url']);
-			return $fields;
-			
+
+	public static function comment_form_default_fields( $fields ) {
+		unset( $fields [ 'url' ] );
+		return $fields;
+	}
+
+	private static function tax_query_name_like( $taxonomy, $term ) {
+		global $wpdb;
+		$statement = $wpdb->prepare(
+		"
+	SELECT	term_taxonomy_id
+	FROM	$wpdb->terms
+	JOIN	$wpdb->term_taxonomy
+	USING	(term_id)
+	WHERE	name
+	LIKE	%s
+	AND		taxonomy = %s
+	", '%' . $term . '%', $taxonomy );
+		return $wpdb->get_col( $statement );
+	}
+
+	public static function pre_get_posts( $query ) {
+
+		if ( is_search() ) {
+			/* @var $author_like string Retorna o nome do autor, caso o usuario tenha usado a busca avancada */
+			$author_like		 = filter_input( INPUT_GET, 'author_like', FILTER_SANITIZE_STRING );
+			/* @var $author_query_string string As palavras-chave da busca normal ou o nome do autor da avancada */
+			$author_query_string = empty( $author_like ) ? get_search_query() : $author_like;
+			/* @var $term_ids array Um vetor com os IDs de todos os autores que possuam nome parecido com o buscado */
+			$term_ids			 = static::tax_query_name_like( 'author', $author_query_string );
+
+			if ( count( $term_ids ) > 0 ) {
+				$author_query_args = array(
+					array(
+						'taxonomy'			 => 'author',
+						'field'				 => 'term_id',
+						'include_children'	 => false,
+						'operator'			 => 'IN',
+						'terms'				 => $term_ids
+					)
+				);
+				$query->set( 'tax_query', $author_query_args );
+			}
 		}
+		return $query;
+	}
+
+	public static function template_redirect() {
+		$current_user_id = get_current_user_id();
+		if ( is_search() && is_user_logged_in() ) {
+			$search_query = get_search_query();
+
+			$last_search_queries = get_user_meta( $current_user_id, 'last_search_queries', true );
+
+			$last_search_queries = apply_filters( 'search_queries', $last_search_queries );
+
+			update_user_meta( $current_user_id, 'last_search_queries', $last_search_queries, false );
+		} if ( is_singular( 'item' ) && is_user_logged_in() ) {
+			$the_id		 = intval( get_the_ID() );
+			$user_meta	 = get_user_meta( $current_user_id, 'last_visited_items', true );
+
+			if ( is_array( $user_meta ) ) {
+
+				$user_meta [] = $the_id;
+			} else {
+				$user_meta = array( $the_id );
+			}
+
+			$user_meta = array_unique( $user_meta );
+			update_user_meta( $current_user_id, 'last_visited_items', $user_meta, false );
+		}
+	}
+
+	public static function search_queries_filter_callback( $search_query ) {
+		return !empty( $search_query ) && strlen( $search_query ) > 1;
+	}
+
+	public static function search_queries( $search_queries ) {
+
+		if ( !is_array( $search_queries ) ) {
+			$search_queries = array( $search_queries );
+		} else {
+			$search_queries = array_unique( $search_queries );
+		}
+		//return array_filter( $search_queries, array( __CLASS__, 'search_queries_filter_callback' ) );
+		return $search_queries;
+	}
+
 }
 
 return new Theme();
